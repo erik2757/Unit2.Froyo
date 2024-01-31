4b2ea6065bc75c28a0adac911eb4817754dd0ee3
Erik Rodriguez <erikrodriguez@Eriks-Air.lan>
2024-01-31 12:05:35 -0600

    followed along with travis

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
-let firstQ = prompt("please enter a list of froyo flavors seperated by commmas")
+ /*
+ 3 vanilla
+ 1 strawberry
+ 2 coffee
+ */ 
 
-let onlineOrder = firstQ.split(",")
-console.log(onlineOrder)
+ const order = prompt("enter flavors here seperated by commas")
 
+ console.log(typeof order)
 
-function froyo (flavorsOrdered){
-    const HowManyFlavors = {}
-    for (let i = 0; i < flavorsOrdered.length; i++)
-   
+ const orderArray = order.split(',')
 
-    if (HowManyFlavors [currentFlavor] === undefined){
-            HowManyFlavors[currentFlavor] = 1;
-    }
-    console.log(HowManyFlavors)
+ console.log(orderArray)
+ // I have an array of flavors at this point 
+
+/*
+{
+'vanilla': 3,
+'coffee': 2, 
+'strawberry': 1,
+}
+*/
+
+let flavorObj = { }
+
+/* HOW TO ADD OBJECTS 
+//adding vanilla 
+// dot notation is preferred when you know the key
+flavorObj.vanilla = 'value'
+flavorObj['vanilla'] = 7 
+
+// dot notation does not work with variable key values 
+// use bracket notation instead 
+flavorObj[MysteryVar] = 'with brackets' 
+flavorObj.MysteryVar = 'with dot'
+*/
+
+// loop to go thru every flavor 
+for(let i = 0; i < orderArray.length; i++) {
+    let currentFlavor = orderArray[i].toLowerCase().trim()
+//   console.log(i, currentFlavor)
+
+    if(flavorObj[currentFlavor] === undefined){
+//if i havent seen it before set to 1 
+//setting it to 1 
+    flavorObj[currentFlavor] = 1 
+} else { 
+// or adding 1 to whats already there
+    flavorObj[currentFlavor] += 1 
+}
 }
 
 
+
+console.log(flavorObj)
---
+++ test.js
@@ -0,0 +1,16 @@
+let firstQ = prompt("please enter a list of froyo flavors seperated by commmas")
+
+let onlineOrder = firstQ.split(",")
+console.log(onlineOrder)
+
+
+function froyo (flavorsOrdered){
+    const HowManyFlavors = {}
+    for (let i = 0; i < flavorsOrdered.length; i++)
+   
+
+    if (HowManyFlavors [currentFlavor] === undefined){
+            HowManyFlavors[currentFlavor] = 1;
+    }
+    console.log(HowManyFlavors)
+}
